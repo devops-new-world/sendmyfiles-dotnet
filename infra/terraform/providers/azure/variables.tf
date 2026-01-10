aae5e74d0f070100b4366a6b7770cbc3ede7124c
Deepak Bomjan <dpaktamang@gmail.com>
2026-01-10 13:05:43 +0545

    Fix Terraform syntax: replace semicolons with proper block formatting in variable definitions

--- infra/terraform/providers/azure/variables.tf
+++ infra/terraform/providers/azure/variables.tf
@@ -1,24 +1,95 @@
-variable "resource_group_name" { type = string }
-variable "location" { type = string }
-variable "environment" { type = string }
-variable "admin_username" { type = string }
-variable "admin_password" { type = string; sensitive = true }
-variable "sql_sa_password" { type = string; sensitive = true }
-variable "minio_root_user" { type = string }
-variable "minio_root_password" { type = string; sensitive = true }
-variable "web_vm_size" { type = string }
-variable "sql_vm_size" { type = string }
-variable "minio_vm_size" { type = string }
-variable "web_vm_disk_size_gb" { type = number }
-variable "sql_vm_disk_size_gb" { type = number }
-variable "minio_vm_disk_size_gb" { type = number }
-variable "sql_data_disk_size_gb" { type = number }
-variable "minio_data_disk_size_gb" { type = number }
-variable "vnet_address_space" { type = list(string) }
-variable "web_subnet_address_prefix" { type = string }
-variable "sql_subnet_address_prefix" { type = string }
-variable "minio_subnet_address_prefix" { type = string }
-variable "ssh_public_key" { type = string; default = "" }
-variable "tags" { type = map(string) }
-variable "random_suffix" { type = string }
+variable "resource_group_name" {
+  type = string
+}
 
+variable "location" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "admin_username" {
+  type = string
+}
+
+variable "admin_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "sql_sa_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "minio_root_user" {
+  type = string
+}
+
+variable "minio_root_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "web_vm_size" {
+  type = string
+}
+
+variable "sql_vm_size" {
+  type = string
+}
+
+variable "minio_vm_size" {
+  type = string
+}
+
+variable "web_vm_disk_size_gb" {
+  type = number
+}
+
+variable "sql_vm_disk_size_gb" {
+  type = number
+}
+
+variable "minio_vm_disk_size_gb" {
+  type = number
+}
+
+variable "sql_data_disk_size_gb" {
+  type = number
+}
+
+variable "minio_data_disk_size_gb" {
+  type = number
+}
+
+variable "vnet_address_space" {
+  type = list(string)
+}
+
+variable "web_subnet_address_prefix" {
+  type = string
+}
+
+variable "sql_subnet_address_prefix" {
+  type = string
+}
+
+variable "minio_subnet_address_prefix" {
+  type = string
+}
+
+variable "ssh_public_key" {
+  type    = string
+  default = ""
+}
+
+variable "tags" {
+  type = map(string)
+}
+
+variable "random_suffix" {
+  type = string
+}
--- infra/terraform/providers/gcp/variables.tf
+++ infra/terraform/providers/gcp/variables.tf
@@ -1,25 +1,99 @@
-variable "project_id" { type = string }
-variable "region" { type = string }
-variable "zone" { type = string }
-variable "environment" { type = string }
-variable "admin_username" { type = string }
-variable "admin_password" { type = string; sensitive = true }
-variable "sql_sa_password" { type = string; sensitive = true }
-variable "minio_root_user" { type = string }
-variable "minio_root_password" { type = string; sensitive = true }
-variable "web_machine_type" { type = string }
-variable "sql_machine_type" { type = string }
-variable "minio_machine_type" { type = string }
-variable "web_disk_size_gb" { type = number }
-variable "sql_disk_size_gb" { type = number }
-variable "minio_disk_size_gb" { type = number }
-variable "sql_data_disk_size_gb" { type = number }
-variable "minio_data_disk_size_gb" { type = number }
-variable "network_cidr" { type = string }
-variable "web_subnet_cidr" { type = string }
-variable "sql_subnet_cidr" { type = string }
-variable "minio_subnet_cidr" { type = string }
-variable "ssh_public_key" { type = string; default = "" }
-variable "tags" { type = map(string) }
-variable "random_suffix" { type = string }
+variable "project_id" {
+  type = string
+}
 
+variable "region" {
+  type = string
+}
+
+variable "zone" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "admin_username" {
+  type = string
+}
+
+variable "admin_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "sql_sa_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "minio_root_user" {
+  type = string
+}
+
+variable "minio_root_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "web_machine_type" {
+  type = string
+}
+
+variable "sql_machine_type" {
+  type = string
+}
+
+variable "minio_machine_type" {
+  type = string
+}
+
+variable "web_disk_size_gb" {
+  type = number
+}
+
+variable "sql_disk_size_gb" {
+  type = number
+}
+
+variable "minio_disk_size_gb" {
+  type = number
+}
+
+variable "sql_data_disk_size_gb" {
+  type = number
+}
+
+variable "minio_data_disk_size_gb" {
+  type = number
+}
+
+variable "network_cidr" {
+  type = string
+}
+
+variable "web_subnet_cidr" {
+  type = string
+}
+
+variable "sql_subnet_cidr" {
+  type = string
+}
+
+variable "minio_subnet_cidr" {
+  type = string
+}
+
+variable "ssh_public_key" {
+  type    = string
+  default = ""
+}
+
+variable "tags" {
+  type = map(string)
+}
+
+variable "random_suffix" {
+  type = string
+}
